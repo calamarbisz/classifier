428982bbdd4faa9ffb652d1609f4567893ded516
calamarbisz <60406035+calamarbisz@users.noreply.github.com>
2020-03-20 14:37:46 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -5,8 +5,8 @@ let label
 function modelready(){
   console.log('modelready');
   mobilenet.predict(video, GotResult);
-
 }
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
   background(0);
@@ -15,15 +15,14 @@ function setup() {
   mobilenet = ml5.imageClassifier('MobileNet',video, modelready)
 }
 
-
 function GotResult(error, results) {
   if (error) {
     console.error(error);
   }
   console.log(results);
   label = results[0].label;
-  
   mobilenet.predict(GotResult);
+  
   fill(0,255,0);
   textSize(30);
   text(label, random(windowWidth), random(windowHeight));
@@ -32,7 +31,7 @@ function GotResult(error, results) {
 
 function draw(){
   imageMode(CENTER);
-  image(video,windowWidth/2*(-1),windowHeight/2,300,200);
+  image(video,windowWidth/2,windowHeight/2,300,200);
   
 }
 
